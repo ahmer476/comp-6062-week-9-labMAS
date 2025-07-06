b3702d44b958e0a607268cc647e728c4e956fc3a
ahmer476 <ahmer873@gmail.com>
2025-07-06 14:22:56 -0400

    NEW
    NEW FILE

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#app',
+    data: {
+        greeting: 'Welcome to Vue.js!',
+        message: 'This message is displayed based on a boolean property.',
+        showMessage: true
+    }
+});
